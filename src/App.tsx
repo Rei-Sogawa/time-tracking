a3dfcf2d36cfb5fef160c1e318314e272e0fff7f
tokita <tokita@sonicgarden.jp>
2021-04-17 02:00:40 +0900

    disabled を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,29 @@
 import React, { useEffect, useState } from 'react';
+import classNames from 'classnames';
 
 export default function App() {
-  const { minutes, seconds, start, stop, clear } = useTimer();
+  const { minutes, seconds, on, start, stop, clear } = useTimer();
 
-  const displayMinutes = () => minutes.toString().padStart(2, '0');
-  const displaySeconds = () => seconds.toString().padStart(2, '0');
+  const displayMinutes = minutes.toString().padStart(2, '0');
+  const displaySeconds = seconds.toString().padStart(2, '0');
 
   return (
     <div className="h-screen bg-gray-100">
       <div className="h-full container mx-auto flex items-center">
         <div className="flex-1 flex-col space-y-10 pb-24">
           <div className="text-center text-9xl font-mono">
-            {displayMinutes()}:{displaySeconds()}
+            {displayMinutes}:{displaySeconds}
           </div>
-          <div className="flex justify-center space-x-3 mx-auto">
-            <XLargeWhiteButton text="START" onClick={() => start()} />
-            <XLargeWhiteButton text="STOP" onClick={() => stop()} />
-            <XLargeWhiteButton
-              text="CLEAR"
-              onClick={() => {
-                clear();
-              }}
-            />
+          <div className="flex justify-center space-x-3">
+            <XLargeWhiteButton onClick={() => start()} disabled={on}>
+              START
+            </XLargeWhiteButton>
+            <XLargeWhiteButton onClick={() => stop()} disabled={!on}>
+              STOP
+            </XLargeWhiteButton>
+            <XLargeWhiteButton onClick={() => clear()} disabled={on}>
+              CLEAR
+            </XLargeWhiteButton>
           </div>
         </div>
       </div>
@@ -29,21 +31,18 @@ export default function App() {
   );
 }
 
-function XLargeWhiteButton({
-  text,
-  onClick,
-}: {
-  text: string;
-  onClick: () => void;
-}) {
+function XLargeWhiteButton(
+  props: React.ButtonHTMLAttributes<HTMLButtonElement>
+) {
   return (
     <button
+      {...props}
       type="button"
-      className="inline-flex items-center px-6 py-3 border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-      onClick={onClick}
-    >
-      {text}
-    </button>
+      className={classNames(
+        'inline-flex items-center px-6 py-3 border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500',
+        { 'opacity-50': props.disabled, 'cursor-not-allowed': props.disabled }
+      )}
+    ></button>
   );
 }
 
@@ -55,10 +54,8 @@ function useTimer() {
   const [on, setOn] = useState(false);
   const start = () => setOn(true);
   const stop = () => setOn(false);
-  const clear = () => {
-    setOn(false);
-    setTotalSeconds(0);
-  };
+  const clear = () => setTotalSeconds(0);
+
   useEffect(() => {
     if (on) {
       const timer = setInterval(() => setTotalSeconds((val) => val + 1), 1000);
@@ -69,6 +66,7 @@ function useTimer() {
   return {
     minutes,
     seconds,
+    on,
     start,
     stop,
     clear,
