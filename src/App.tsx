53d94e01a4f9eec1b1796850c6a9880e515ef65c
tokita <tokita@sonicgarden.jp>
2021-04-17 00:58:31 +0900

    timer を custom hooks 化

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,28 @@
 import React, { useEffect, useState } from 'react';
 
 export default function App() {
-  const [totalSeconds, setTotalSeconds] = useState(0);
-  const [on, setOn] = useState(false);
+  const { minutes, seconds, start, stop, clear } = useTimer();
 
-  useEffect(() => {
-    if (on) {
-      const timer = setInterval(() => setTotalSeconds((val) => val + 1), 1000);
-      return () => clearInterval(timer);
-    }
-  }, [on]);
-
-  const minutes = () => Math.floor(totalSeconds / 60);
-  const seconds = () => totalSeconds - minutes() * 60;
-
-  const displayMinutes = () => minutes().toString().padStart(2, '0');
-  const displaySeconds = () => seconds().toString().padStart(2, '0');
+  const displayMinutes = () => minutes.toString().padStart(2, '0');
+  const displaySeconds = () => seconds.toString().padStart(2, '0');
 
   return (
-    <div className="h-screen bg-gray-100 flex items-center">
-      <div className="flex-1 flex-col space-y-10 pb-24">
-        <div className="text-center text-9xl font-mono">
-          {displayMinutes()}:{displaySeconds()}
-        </div>
-        <div className="flex justify-center space-x-3 mx-auto">
-          <XLargeWhiteButton text="START" onClick={() => setOn(true)} />
-          <XLargeWhiteButton text="STOP" onClick={() => setOn(false)} />
-          <XLargeWhiteButton
-            text="CLEAR"
-            onClick={() => {
-              setOn(false);
-              setTotalSeconds(0);
-            }}
-          />
+    <div className="h-screen bg-gray-100">
+      <div className="h-full container mx-auto flex items-center">
+        <div className="flex-1 flex-col space-y-10 pb-24">
+          <div className="text-center text-9xl font-mono">
+            {displayMinutes()}:{displaySeconds()}
+          </div>
+          <div className="flex justify-center space-x-3 mx-auto">
+            <XLargeWhiteButton text="START" onClick={() => start()} />
+            <XLargeWhiteButton text="STOP" onClick={() => stop()} />
+            <XLargeWhiteButton
+              text="CLEAR"
+              onClick={() => {
+                clear();
+              }}
+            />
+          </div>
         </div>
       </div>
     </div>
@@ -55,4 +45,32 @@ function XLargeWhiteButton({
       {text}
     </button>
   );
+}
+
+function useTimer() {
+  const [totalSeconds, setTotalSeconds] = useState(0);
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = totalSeconds - minutes * 60;
+
+  const [on, setOn] = useState(false);
+  const start = () => setOn(true);
+  const stop = () => setOn(false);
+  const clear = () => {
+    setOn(false);
+    setTotalSeconds(0);
+  };
+  useEffect(() => {
+    if (on) {
+      const timer = setInterval(() => setTotalSeconds((val) => val + 1), 1000);
+      return () => clearInterval(timer);
+    }
+  }, [on]);
+
+  return {
+    minutes,
+    seconds,
+    start,
+    stop,
+    clear,
+  };
 }
