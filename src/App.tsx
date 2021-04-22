f7b11387da2b3bad97dac61b3796ebe6e4f13fbb
tokita <tokita@sonicgarden.jp>
2021-04-22 23:25:01 +0900

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import classNames from 'classnames';
-import { SubmitHandler, useForm } from 'react-hook-form';
 
 export default function App() {
-  const { minutes, seconds, on, start, stop, clear } = useTimer();
-
+  const { seconds: totalSecond, on, start, stop, clear } = useTimer();
+  const minutes = Math.floor(totalSecond / 60);
+  const seconds = totalSecond - minutes * 60;
   const displayMinutes = minutes.toString().padStart(2, '0');
   const displaySeconds = seconds.toString().padStart(2, '0');
 
@@ -29,13 +29,6 @@ export default function App() {
               DONE
             </XLargeWhiteButton>
           </div>
-          <div>
-            <TaskForm
-              onSubmit={(values) => {
-                console.log(values);
-              }}
-            />
-          </div>
         </div>
       </div>
     </div>
@@ -59,59 +52,25 @@ function XLargeWhiteButton(
 }
 
 function useTimer() {
-  const [totalSeconds, setTotalSeconds] = useState(0);
-  const minutes = Math.floor(totalSeconds / 60);
-  const seconds = totalSeconds - minutes * 60;
+  const [seconds, setSeconds] = useState(0);
 
   const [on, setOn] = useState(false);
   const start = () => setOn(true);
   const stop = () => setOn(false);
-  const clear = () => setTotalSeconds(0);
+  const clear = () => setSeconds(0);
 
   useEffect(() => {
     if (on) {
-      const timer = setInterval(() => setTotalSeconds((val) => val + 1), 1000);
+      const timer = setInterval(() => setSeconds((val) => val + 1), 1000);
       return () => clearInterval(timer);
     }
   }, [on]);
 
   return {
-    minutes,
     seconds,
     on,
     start,
     stop,
     clear,
   };
-}
-
-const TextInput = React.forwardRef<
-  HTMLInputElement,
-  React.InputHTMLAttributes<HTMLInputElement>
->((props: React.InputHTMLAttributes<HTMLInputElement>, ref) => (
-  <input
-    type="text"
-    className={classNames(
-      'block w-full border border-gray-300 rounded-md shadow-sm px-6 py-3 text-base font-medium text-gray-700 placeholder-gray-400 bg-white focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500',
-      props.className
-    )}
-    {...props}
-    ref={ref}
-  />
-));
-
-type TaskFormValues = { taskName: string };
-
-function TaskForm({ onSubmit }: { onSubmit: SubmitHandler<TaskFormValues> }) {
-  const { register, handleSubmit, reset } = useForm();
-  return (
-    <form
-      onSubmit={handleSubmit((data: TaskFormValues) => {
-        onSubmit(data);
-        reset();
-      })}
-    >
-      <TextInput {...register('taskName')} placeholder="新しいタスク" />
-    </form>
-  );
 }
