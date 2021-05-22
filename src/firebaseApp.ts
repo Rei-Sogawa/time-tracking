e9c216479dfa672d0942d63ef94940ff8f278df9
Rei-Sogawa <natto.and.eggs@gmail.com>
2021-05-22 22:02:23 +0900

    prettierrc と simple-import-sort を設定。firebase install もした

--- src/firebaseApp.ts
+++ src/firebaseApp.ts
@@ -1,14 +1,15 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
+import 'firebase/firestore';
+
+import firebase from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAbEcwQm6gym4qPYLO-grpFvwoutSF-TsI",
-  authDomain: "time-tracking-267fa.firebaseapp.com",
-  projectId: "time-tracking-267fa",
-  storageBucket: "time-tracking-267fa.appspot.com",
-  messagingSenderId: "981000197641",
-  appId: "1:981000197641:web:c87cd164d3ee078dca8ad8",
-  measurementId: "G-S93XLG9DTE",
+  apiKey: 'AIzaSyAbEcwQm6gym4qPYLO-grpFvwoutSF-TsI',
+  authDomain: 'time-tracking-267fa.firebaseapp.com',
+  projectId: 'time-tracking-267fa',
+  storageBucket: 'time-tracking-267fa.appspot.com',
+  messagingSenderId: '981000197641',
+  appId: '1:981000197641:web:c87cd164d3ee078dca8ad8',
+  measurementId: 'G-S93XLG9DTE',
 };
 
 if (!firebase.apps.length) {
